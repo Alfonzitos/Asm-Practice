94396e106176246ace55fbab18c84f3b4284ad85
Alfonzitos <nylenalfons@gmail.com>
2023-01-07 15:55:38 +0100

    Prints 0-9 of the same characters depending on single digit input

---
+++ print_shapes.asm
@@ -0,0 +1,70 @@
+section .data
+NULL equ 0
+SYS_write equ 1
+SYS_read equ 0
+SYS_exit equ 60
+STDIN equ 0
+STDOUT equ 1
+Char db "*"
+CharInput db "0"
+
+section .text
+global _start
+_start:
+
+mov rdi, CharInput
+mov rsi, 1
+call read_stdin
+
+mov rdi, CharInput
+call char_to_int
+
+mov r8b, byte [CharInput]
+mov r9, 0
+print_loop:
+cmp r9b, r8b
+je print_loop_done
+
+mov rax, SYS_write
+mov rdi, STDOUT
+mov rsi, Char
+mov rdx, 1
+syscall
+
+inc r9
+jmp print_loop
+
+
+print_loop_done:
+
+
+
+mov rax, SYS_exit
+mov rdi, 0
+syscall
+
+global read_stdin
+read_stdin:
+;arg1 address to read to, arg2 amount of characters to read
+push rbx
+push rbp
+mov rbx, rdi
+mov rbp, rsi
+
+mov rax, SYS_read
+mov rdi, STDIN
+mov rsi, rbx
+mov rdx, rbp
+syscall
+
+pop rbp
+pop rbx
+
+ret
+
+global char_to_int
+char_to_int:
+;arg1 character address to convert
+sub byte [rdi], 48
+ret
+
