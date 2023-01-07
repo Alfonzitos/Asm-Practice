68790224ffdcc2d4362dfd8880e9fbbca2c431be
Alfonzitos <nylenalfons@gmail.com>
2023-01-08 00:50:50 +0100

    Modified script to generate debug info that can be used in GDB

--- CompileAsm.sh
+++ CompileAsm.sh
@@ -4,5 +4,5 @@ file=$1
 file_no_extension=${file%.*}
 file_exe_extension="${file_no_extension}.exe"
 file_object_extension="${file_no_extension}.o"
-nasm -f elf64 $file
+nasm -g -f elf64 $file
 ld $file_object_extension -o $file_exe_extension
